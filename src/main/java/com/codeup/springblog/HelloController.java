8e06a98fd9f903b4df55cb7e3b0b3f091716ae65
Juan Longoria <juan.a.longoria66@gmail.com>
2022-02-24 10:03:42 -0600

    completed views lec

--- src/main/java/com/codeup/springblog/HelloController.java
+++ src/main/java/com/codeup/springblog/HelloController.java
@@ -1,6 +1,7 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -24,6 +25,31 @@ public class HelloController {
     @ResponseBody
     public String increment(@PathVariable int number) {
         return number + " plus one is " + (number + 1) + "!";
+    }
+
+
+
+    // Example of Receiving and Sending Data
+
+    @GetMapping("/join")
+    public String showJoinForm() {
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model) {
+        model.addAttribute("cohort", "Welcome to " + cohort + "!");
+        return "join";
+    }
+
+
+
+    // Passing a collection of data
+    @GetMapping("/greek-gods")
+    public String showGreekGods(Model model) {
+        String[] names = {"Zeus", "Hercules", "Hades", "Apollo"};
+        model.addAttribute("greekGods", names);
+        return "greekGods";
     }
 
 
