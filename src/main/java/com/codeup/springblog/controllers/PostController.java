ca9de734c4ff915a0495f9708283a5087b1b9928
Juan Longoria <juan.a.longoria66@gmail.com>
2022-02-28 16:53:55 -0600

    reposotories and jpa exercise

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -41,18 +41,25 @@
 package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.models.Post;
+import com.codeup.springblog.repositories.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-public class PostController {
+public class PostController{
+    private PostRepository postsDao;
+
+    public PostController(PostRepository postsDao) {
+        this.postsDao = postsDao;
+    }
+
     @GetMapping("/posts")
     public String viewPosts(Model model) {
         List<Post> allPosts = new ArrayList<>();
@@ -82,9 +89,53 @@ public class PostController {
 
 
     @PostMapping("/posts/create")
-    @ResponseBody
-    public String submitCreateForm() {
-        return "create a new post";
+//    @ResponseBody
+//    public String submitCreateForm() {
+//        return "create a new post";
+//    }
+    public String submitCreateForm(@RequestParam(name = "title") String title, @RequestParam(name = "body") String body) {
+        Post newPost = new Post(title, body);
+        postsDao.save(newPost);
+
+        return "redirect:/posts";
+    }
+
+    @GetMapping("/posts/{id}/edit")
+    public String showEditForm(@PathVariable long id, Model model) {
+        Post posttoEdit = postsDao.getById(id);
+        model.addAttribute("postToEdit", posttoEdit);
+        return "posts/edit";
+    }
+
+    //We can access the values submitted from the form using our @RequestParam annotation
+    @PostMapping("/posts/{id}/edit")
+    public String submitEdit(@RequestParam(name = "title") String title, @RequestParam(name = "body") String body, @PathVariable long id) {
+//        Post postToEdit = postsDao.getById(id);
+//        postToEdit.setTitle(title);
+//        postToEdit.setBody(body);
+//        postsDao.save(postToEdit);
+//        return "redirect:/posts";
+//    }
+
+        // grab the post from our DAO
+        Post postToEdit = postsDao.getById(id);
+        // use setters to set new values to the object
+        postToEdit.setTitle(title);
+        postToEdit.setBody(body);
+        // save the object with new values
+        postsDao.save(postToEdit);
+        return "redirect:/posts";
+    }
+
+    // For now, we need to use a GetMapping, that way, when we visit the page,
+    // our app can access the path variable, then delete the post, then redirect
+    // us back to the post index page.
+
+
+    @GetMapping("/posts/{id}/delete")
+    public String delete(@PathVariable long id) {
+        postsDao.deleteById(id);
+        return "redirect:/posts";
     }
 }
 
